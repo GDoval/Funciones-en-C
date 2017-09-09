973fdc130ccf0c12dc17bcdc286502d71116bdf8
GDoval <charly_sxxi@hotmail.com>
2017-09-09 14:20:21 -0300

    Funciones para vectores

---
+++ Linux/Vectores/Vectores.c
@@ -0,0 +1,22 @@
+
+/**brief Ordena un vector de menor a mayor
+/*param recibe el puntero de un vector
+/*param  recibe un int con  la longitud del vector.
+
+
+void ordenarVecMenor (int vector[], int leng)
+{
+    int i, j;
+    int temporal;
+    for (i = 1; i < leng; i++)
+    {
+        temporal = vector[i];
+        j = i-1;
+        while(temporal < vector[j]  && j >= 0)
+        {
+            vector[j+1] = vector[j];
+            j--;
+        }
+        vector[j+1] = temporal;
+    }
+}
