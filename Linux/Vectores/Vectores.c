274d802ab9d55915d841cf512183875dccff3e7e
GDoval <charly_sxxi@hotmail.com>
2017-09-09 14:53:59 -0300

    Funciones de vectores

--- Linux/Vectores/Vectores.c
+++ Linux/Vectores/Vectores.c
@@ -46,3 +46,71 @@ void ordenarVecMayor (int vector[], int leng)
         vector[j+1] = temporal;
     }
 }
+
+
+--------------------------------------------------------------------------------------
+
+/**brief devuelve el mayor numero dentro de un vector.
+/*param recibe un vector
+/*recibe la longitud del vector.
+/* return devuelve un int
+/**
+
+int buscarMayor (int vector[], int leng)
+{
+    int mayor = vector[0];
+    for(int i = 0; i < leng; i++)
+    {
+        if (vector[i] > mayor)
+        {
+            mayor = vector[i];
+        }
+    }
+    return mayor;
+}
+
+
+---------------------------------------------------------------------------------------
+/**
+/*brief devuelve el menor numero dentro de un vector
+/*param recibe un vector
+/*recibe  la longitud del vector
+/*devuelve un int 
+
+int buscarMenor (int vector[], int leng)
+{
+    int menor = vector[0];
+    for(int i = 0; i < leng; i++)
+    {
+        if (vector[i] > menor)
+        {
+            menor = vector[i];
+        }
+    }
+    return menor;
+}
+
+-----------------------------------------------------------------------------------------
+
+/**
+/*brief  busca un numero en el vector
+/*param recibe un vector
+/*param recibe la longitud del vector
+/*param recibe el numero a buscar
+/*return devuelve un char 'n' si el numero no se encuentra en el vector, y un char 's' en caso contrario.
+
+
+char buscarNumero (int vector[], int leng, int numero)
+{
+    char resultado = 'n';
+    for (int i = 0; i < leng; i++)
+    {
+        if (vector[i] == numero)
+        {
+            resultado = 's';
+            return resultado;
+        }
+    }
+    return resultado;
+}
+
